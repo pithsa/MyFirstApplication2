a908ba26f7ad3e8c6c83956fea9a2dce2acdf7f4
pithsa <852133883@qq.com>
2023-12-06 11:07:10 +0800

    3

--- MainActivity.java
+++ MainActivity.java
@@ -1,3 +1,9 @@
 public class MainActivity {
     private int a = 1;
+    <resources>
+    <string name="hello">hello world!</string>
+</resources>
+    <resources>
+    <string name="hello">hello world!</string>
+</resources>
 }
